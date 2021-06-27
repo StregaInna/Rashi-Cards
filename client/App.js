640829cd1c34ec5437fc96eedd23d165489cba0f
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-06-27 11:13:10 -0400

    refactored keboard passing props directly instead of using redux

--- client/App.js
+++ client/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+// import Navbar from './components/Navbar'
+// import Routes from './Routes'
 import FlashCard from './components/FlashCards'
-import HebrewKeyboard from './components/Keyboard'
+// import HebrewKeyboard from './components/Keyboard'
 
 const App = () => {
   return (
     <div className="App">
         <FlashCard/>
-        <HebrewKeyboard/>
     </div>
   )
 }
--- client/components/FlashCards.js
+++ client/components/FlashCards.js
@@ -3,12 +3,16 @@ import { connect } from 'react-redux';
 import { clearString, realKeyboard } from '../store/inputString'
 import { loadRashiScript, nextCard, reAddCard, shuffleDeck } from '../store/cardDeck';
 import { correctAnswer, incorrectAnswer, resetScore } from '../store/score';
+import Keyboard from './Keyboard'
 
 
 
 class FlashCard extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            inputString: ''
+        }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
     }
@@ -16,9 +20,10 @@ class FlashCard extends React.Component {
         this.props.loadRashiScript()
         this.props.shuffle()
     }
-
     handleChange(event) {
-      this.props.realKeyboard(event.target.value)
+        this.setState({
+            inputString: event.target.value
+        })
     }
 
     handleSubmit(event) {
@@ -34,7 +39,7 @@ class FlashCard extends React.Component {
       }
 
     render(){
-        console.log(`ths input string is ${this.props.inputString}`)
+        console.log(this.state)
         return <div>
             {this.props.cardDeck?(
             <div id="flashcard-div">
@@ -45,7 +50,7 @@ class FlashCard extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                         <label>
                             Letter:
-                            <input type="text" value={this.props.inputString} onChange={this.handleChange} />
+                            <input type="text" value={this.state.inputString} onChange={this.handleChange} />
                         </label>
                         <input type="submit" value="Submit" />
                     </form>
@@ -55,29 +60,32 @@ class FlashCard extends React.Component {
                 </div>
             </div>):(<h1>Loading...</h1>)
             }
+          <Keyboard clickHandler={(event)=>this.handleChange(event)}/>  
         </div>
 
     }
 }
 const mapState = (state) => {
     return {
-        inputString: state.inputString,
+        //inputString: state.inputString,
         cardDeck: state.cards.cardDeck,
         currentCard: state.cards.currentCard,
         cardIndex: state.cards.cardIndex,
         score: state.score
     }
 }
-const mapDispatch = (dispatch) => ({
-    realKeyboard: (string) => dispatch(realKeyboard(string)),
-    loadRashiScript: () => dispatch(loadRashiScript()),
-    shuffle: () => dispatch(shuffleDeck()),
-    nextCard: () => dispatch(nextCard()),
-    reAddCard: () => dispatch(reAddCard()),
-    clearString: () => dispatch(clearString()),
-    correctAnswer: () => dispatch(correctAnswer()),
-    incorrectAnswer: () => dispatch(incorrectAnswer()),
-    resetScore: () => dispatch(resetScore())
+const mapDispatch = (dispatch) => {
+    return{
+        realKeyboard: (string) => dispatch(realKeyboard(string)),
+        loadRashiScript: () => dispatch(loadRashiScript()),
+        shuffle: () => dispatch(shuffleDeck()),
+        nextCard: () => dispatch(nextCard()),
+        reAddCard: () => dispatch(reAddCard()),
+        clearString: () => dispatch(clearString()),
+        correctAnswer: () => dispatch(correctAnswer()),
+        incorrectAnswer: () => dispatch(incorrectAnswer()),
+        resetScore: () => dispatch(resetScore())
+    }
 
-})
+}
 export default connect(mapState, mapDispatch)(FlashCard)
